354602fac43b2b7e5d639d515c9768e83e2997e9
TooniOlaniyan <asalamlatif@gmail.com>
2024-03-14 11:31:06 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Button from './components/Button';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl bg-red-500 font-bold underline">Hello world!</h1>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Button className='mt-10'>
+          Something
+
+        </Button>
+
+      </div>
+      {/* <ButtonGradient/> */}
     </>
   );
 }
